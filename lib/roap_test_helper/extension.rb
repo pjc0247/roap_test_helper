7e650a8e1ef2dee66530ca1e1081a4aa48e0d0b1
Park June Chul <pjc0247@naver.com>
2015-07-02 17:48:07 +0900

    Update extension.rb

--- lib/roap_test_helper/extension.rb
+++ lib/roap_test_helper/extension.rb
@@ -1,3 +1,5 @@
+require 'ripper'
+
 module Roap
   module TestHelper
     module Extension
@@ -9,10 +11,30 @@ module Roap
         should = eval md[:should].to_s
 
         TestHelper::tests.push({
-          :klass => method.receiver,
+          :type => :singleton_method,
+          :klass => klass,
           :method_name => method.name,
           :args => args,
           :should => should})
+      end
+
+      #static
+      on capture_under("@example") do |klass, method, md|
+        body = Roap::Utils::decomment md[:body]
+        last = Ripper.lex(body.lines.last).last
+
+        if last[1] == :on_comment
+          last[2].gsub! /^#*/, ""
+          
+          should = eval last[2]
+
+          TestHelper::tests.push({
+            :type => :code,
+            :klass => klass,
+            :method_name => method.name,
+            :should => should,
+            :code => body})
+        end
       end
     end
   end
